e4a1cffb47576b2a26456dc19b1540c9388c1505
Dan Marshall <danmar@microsoft.com>
2017-12-31 00:35:24 -0800

    add @jscad/scad-api common dependency

--- js/worker.js
+++ js/worker.js
@@ -1,6 +1,7 @@
 //Common interfaces
 var commonDependencyUrls = {
     "@jscad/csg": "@jscad/csg/0.3.7.js",
+    "@jscad/scad-api": "@jscad/scad-api/0.4.2.js",
     "makerjs": "makerjs/0.9.78.js"
 };
 var _module;
--- src/worker/manifest.ts
+++ src/worker/manifest.ts
@@ -1,4 +1,5 @@
 let commonDependencyUrls = {
   "@jscad/csg": "@jscad/csg/0.3.7.js",
+  "@jscad/scad-api": "@jscad/scad-api/0.4.2.js",
   "makerjs": "makerjs/0.9.78.js"
 };
